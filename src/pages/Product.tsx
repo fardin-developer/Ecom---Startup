592b9dec6a04012b19c2f67aff5324ebc3cd4fa4
fardin379 <nrdsraz@gmail.com>
2024-03-29 01:14:42 +0530

    original products added

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -2,13 +2,25 @@ import ProductCard from '../component/ProductCard'
 
 const Product = () => {
   const products = [
-    { id: 0, name: 'Product 1', price: '₹599' },
-    { id: 1, name: 'Product 2', price: '₹799' },
-    { id: 2, name: 'Product 3', price: '₹999' }
+    { id: 0, name: 'Wheat', price: '₹2000 per quintal' },
+    { id: 1, name: 'Rice', price: '₹2500 per quintal' },
+    { id: 2, name: 'Corn', price: '₹1800 per quintal' },
+    { id: 3, name: 'Barley', price: '₹2200 per quintal' },
+    { id: 4, name: 'Soybean', price: '₹3200 per quintal' },
+    { id: 5, name: 'Sugarcane', price: '₹2500 per ton' },
+    { id: 6, name: 'Cotton', price: '₹4000 per quintal' },
+    { id: 7, name: 'Potato', price: '₹20 per kg' },
+    { id: 8, name: 'Tomato', price: '₹30 per kg' },
+    { id: 9, name: 'Onion', price: '₹25 per kg' },
+    { id: 10, name: 'Carrot', price: '₹35 per kg' },
+    { id: 11, name: 'Cauliflower', price: '₹40 per piece' },
+    { id: 12, name: 'Cabbage', price: '₹20 per kg' },
+    { id: 13, name: 'Brinjal', price: '₹25 per kg' },
+    { id: 14, name: 'Pumpkin', price: '₹30 per kg' }
   ]
 
   return (
-    <div className='grid sm:grid-cols-3 grid-cols-2 gap-4'>
+    <div className='grid lg:grid-cols-3 grid-cols-2 gap-4'>
       {products.map((product, index) => (
         <ProductCard key={index} id={product.id} name={product.name} price={product.price} />
       ))}
